312b57105fb9a831b994073f922ce7973b6a1317
Matteo Purici <smatteo.purici@itis.pr.it>
2024-02-29 22:49:17 +0100

    form completo

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-function validateAge() {
-    
-    var age = document.getElementById('age').value;
-    if (isNaN(age) || age <= 0 || age >100) {
-        document.getElementById('ageError').innerText = "Inserire un'età valida.";
+
+const regexName = /^[A-Za-z]+$/;
+const regexDateOfBirth = /^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-\d{4}$/;
+const regexPhoneNumber = /^\+\d{10,15}$/;
+const regexStreetNumber = /^\d+[a-zA-Z]*$/;
+const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+function validateField(fieldId, regex, errorMessage) {
+    var value = document.getElementById(fieldId).value;
+    var errorElement = document.getElementById(fieldId + 'Error');
+
+    if (regex.test(value)) {
+        errorElement.innerText = "";
     } else {
-        document.getElementById('ageError').innerText = "";
+        errorElement.innerText = errorMessage;
     }
 }
 
+function validateForm() {
+    validateField('name', regexName, "Inserire un nome valido.");
+    validateField('surname', regexName, "Inserire un cognome valido.");
+    validateField('dob', regexDateOfBirth, "Inserire una data di nascita valida.");
+    validateField('phone', regexPhoneNumber, "Inserire un numero di telefono valido.");
+    validateField('address', /.+/, "Inserire un indirizzo.");
+    validateField('streetNumber', regexStreetNumber, "Inserire un numero civico valido.");
+    validateField('email', regexEmail, "Inserire un indirizzo email valido.");
+}
+
 function submitForm() {
-    validateAge();
-    if (document.getElementById('ageError').innerText === "") {
+    validateForm();
+    var errorElements = document.querySelectorAll('.error-message');
+    var hasErrors = Array.from(errorElements).some(element => element.innerText !== "");
+
+    if (!hasErrors) {
         // Invia il form o esegui altre azioni necessarie
         alert("Form inviato con successo!");
     } else {
